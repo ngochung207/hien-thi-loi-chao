3b6d4a6ad836fcdcec6995ce71674bcd2887c285
ngochung207 <ngochung207@gmail.com>
2020-12-09 16:07:09 +0700

    update code

---
+++ src/hien_thi_loi_chao.java
@@ -0,0 +1,12 @@
+import sun.security.x509.InvalidityDateExtension;
+
+import java.util.Scanner;
+
+public class hien_thi_loi_chao {
+    public static void main(String[] args) {
+        System.out.println("Nhap vao ten:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello: " + name );
+    }
+}
